cd201ec4b20fbec392f9386a951a7ea60ee59754
Louis Moses <louismoses.rait@gmail.com>
2023-01-06 05:49:18 +0800

    added CSS

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let firstCard = Math.floor(Math.random() * (12 - 2) + 2);
-let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
-
+let firstCard = 10; //Math.floor(Math.random() * (12 - 2) + 2);
+let secondCard = 11; //Math.floor(Math.random() * (12 - 2) + 2);
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
 console.log(sum);
-if (sum < 21) {
-  console.log("waw");
+if (sum <= 20) {
+  message = "Do you want to draw another card?";
+} else if (sum === 21) {
+  message = "Wohoo! You've got Blackjack";
+  hasBlackJack = true;
+} else {
+  message = "You're out of the game";
+  isAlive = false;
 }
+
+// CASH OUT
+console.log(hasBlackJack);
+console.log(isAlive);
+console.log(message);
+
+// scratch section
+
+// end scratch section
