9d325ce3c47dcdba264b329bff450421e670337c
Louis Moses <louismoses.rait@gmail.com>
2023-01-06 06:10:11 +0800

    added Start button

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-console.log(sum);
-if (sum <= 20) {
-  message = "Do you want to draw another card?";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game";
-  isAlive = false;
-}
+function startGame() {
+  console.log("game started");
+  if (sum <= 20) {
+    message = "Do you want to draw another card?";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game";
+    isAlive = false;
+  }
 
-// CASH OUT
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+  console.log(sum);
+
+  // CASH OUT
+  console.log(hasBlackJack);
+  console.log(isAlive);
+  console.log(message);
+}
 
 // scratch section
 
