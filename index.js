8b54c9410ac84e50c9934c2ba99affff856aa68b
Louis Moses <louismoses.rait@gmail.com>
2023-01-06 06:19:46 +0800

    button has startGame function

--- index.js
+++ index.js
@@ -4,25 +4,20 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
 
 function startGame() {
-  console.log("game started");
   if (sum <= 20) {
     message = "Do you want to draw another card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack";
+    message = "You've got Blackjack";
     hasBlackJack = true;
   } else {
     message = "You're out of the game";
     isAlive = false;
   }
 
-  console.log(sum);
-
-  // CASH OUT
-  console.log(hasBlackJack);
-  console.log(isAlive);
-  console.log(message);
+  messageEl.textContent = message;
 }
 
 // scratch section
