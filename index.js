09d9d0f41c32d3fcc3cc9ccf96f8e8a61980d946
Louis Moses <louismoses.rait@gmail.com>
2023-01-10 10:34:24 +0800

    convert player name and credits to object

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector(".sum-el");
 let cardEl = document.querySelector(".card-el");
 
-let playerName = "Moses";
-let playerChips = 145;
+let player = {
+  name: "Moses",
+  chips: 145,
+};
 
 let playerEl = document.getElementById("player-el");
-playerEl.textContent = playerName + ": $" + playerChips;
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13 + 1);
